1ab613062464be520e7c7144bfd0fbf26d03a231
Logan Jones <loganemilyjones@mac.com>
2021-01-12 14:15:43 -0800

    Node and Stack base classes

---
+++ python/data_structures/stacks_and_queues/stacks_and_queues.py
@@ -0,0 +1,9 @@
+class Node():
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+class Stack():
+    def __init__(self, top=None):
+        self.top = top
+
