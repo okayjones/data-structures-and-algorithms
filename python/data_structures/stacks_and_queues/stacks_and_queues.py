a79ada6ccf5226f9e12fdb8413c21eb71f249deb
Logan Jones <loganemilyjones@mac.com>
2021-01-12 14:40:24 -0800

    stack methods done

--- python/data_structures/stacks_and_queues/stacks_and_queues.py
+++ python/data_structures/stacks_and_queues/stacks_and_queues.py
@@ -7,3 +7,23 @@ class Stack():
     def __init__(self, top=None):
         self.top = top
 
+    def push(self, value):
+        new = Node(value, self.top)
+        self.top = new
+
+    def pop(self):
+        if self.isEmpty():
+            raise IndexError("Stack is empty")
+        else:
+            value = self.top.value
+            self.top = self.top.next
+            return value
+
+    def peek(self):
+        if self.isEmpty():
+            raise IndexError("Stack is empty")
+        else:
+            return self.top.value
+
+    def isEmpty(self):
+        return True if not self.top else False
