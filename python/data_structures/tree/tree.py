d609ab8cd4201c85d4f08fd92cedc135bf8940aa
Logan Jones <loganemilyjones@mac.com>
2021-01-30 12:13:43 -0800

    Node and BinaryTree done

--- python/data_structures/tree/tree.py
+++ python/data_structures/tree/tree.py
@@ -1,23 +1,47 @@
-class Node():
-    pass
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
-class BinaryTree():
-    def __init__():
-        pass
-    
-    def preOrder() -> list:
-        pass
+class BinaryTree:
+    def __init__(self, root=None):
+        self.root = root
 
-    def inOrder() -> list:
-        pass
-    
-    def postOrder() -> list:
-        pass
+    def pre_order(self) -> list:
+        result = []
+        def traverse(root):
+            result.append(root.value)
+            if root.left:
+                traverse(root.left)
+            if root.right:
+                traverse(root.right)
+        traverse(self.root)
+        return result
 
-class BinarySearchTree():
-    def __init__():
-        pass
-    
+    def in_order(self) -> list:
+        result = []
+        def traverse(root):
+            if root.left:
+                traverse(root.left)
+            result.append(root.value)
+            if root.right:
+                traverse(root.right)
+        traverse(self.root)
+        return result
+
+    def post_order(self) -> list:
+        result = []
+        def traverse(root):
+            if root.left:
+                traverse(root.left)
+            if root.right:
+                traverse(root.right)
+            result.append(root.value)
+        traverse(self.root)
+        return result
+
+class BinarySearchTree(BinaryTree):
     def add():
         pass
     
