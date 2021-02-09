22e96e4cbd246ffe38182c406f5a53473f168396
Logan Jones <loganemilyjones@mac.com>
2021-02-09 08:27:25 -0800

    initial test

--- python/challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ python/challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -1,20 +1,21 @@
 def fizz_buzz_tree(tree):
+    def fizz_buzz(value):
+        if value % 15 == 0:
+            return 'FizzBuzz'
+        elif value % 3 == 0:
+            return 'Fizz'
+        elif value % 5 == 0:
+            return 'Buzz'
+        else:
+            return str(value)
+
     def traverse(root):
         root.value = fizz_buzz(root.value)
         if root.left:
             traverse(root.left)
         if root.right:
             traverse(root.right)
-    traverse(self.root)
-
-    def fizz_buzz(value):
-        if value % 15:
-            return 'FizzBuzz'
-        elif value % 3:
-            return 'Fizz'
-        elif value % 5:
-            return 'Buzz'
-        else:
-            return str(value)
+    traverse(tree.root)
 
     return tree
+
--- python/tests/test_fizz_buzz_tree.py
+++ python/tests/test_fizz_buzz_tree.py
@@ -1,4 +1,48 @@
-from challenges.fizz_buzz_tree import __version__
+import pytest
+from challenges.fizz_buzz_tree.fizz_buzz_tree import fizz_buzz_tree
+from data_structures.tree.tree import Node, BinaryTree
 
-def test_version():
-    assert __version__ == "0.1.0"
+
+def test_fizz_buzz(tree, fb_tree):
+    actual = fizz_buzz_tree(tree).pre_order()
+    expected = fb_tree.pre_order()
+    assert actual == expected
+
+
+@pytest.fixture
+def tree():
+    tree = BinaryTree()
+    a = Node(30)
+    b = Node(10)
+    c = Node(7)
+    d = Node(3)
+    e = Node(11)
+    f = Node(21)
+
+    tree.root = a
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.left = f
+
+    return tree
+
+@pytest.fixture
+def fb_tree():
+    tree = BinaryTree()
+    a = Node('FizzBuzz')
+    b = Node('Buzz')
+    c = Node('7')
+    d = Node('Fizz')
+    e = Node('11')
+    f = Node('Fizz')
+
+    tree.root = a
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.left = f
+
+    return tree
