24887b456bec0754be6371baa92bdc8bf78ad1b4
Logan Jones <loganemilyjones@mac.com>
2021-03-22 19:50:58 -0700

    tests

--- python/challenges/left_join/left_join.py
+++ python/challenges/left_join/left_join.py
@@ -1,2 +1,18 @@
-def left_join():
-    pass
+from data_structures.hashtable.hashtable import Hashtable
+
+
+def left_join(ht_left, ht_right):
+    result = []
+
+    for ll in ht_left._buckets:
+        if ll.display():
+            key, value = ll.display().pop()
+            row = [key, value]
+
+            if ht_right.contains(key):
+                row.append(ht_right.get(key))
+            else:
+                row.append(None)
+            result.append(row)
+
+    return result
