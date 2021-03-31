bc66a5ec0ab1a6b8037dd78f45250c17b5157c78
Logan Jones <loganemilyjones@mac.com>
2021-03-31 08:06:48 -0700

    simplified

--- python/challenges/get_edge/get_edge.py
+++ python/challenges/get_edge/get_edge.py
@@ -2,28 +2,22 @@ from data_structures.graph.graph import Graph
 
 
 def get_edge(graph, route):
-
-    # exit immeditaely if either are empty
     if graph.get_nodes() == None or route == None:
         return False, 0
 
     total_weight = 0
 
-    # iterate through route
-    for index, city in enumerate(route):
-        # Connected_edges is a list of Edges (self.vertex, self.weight)
-        connected_cities = graph.get_neighbors(city)
-        route_cities = [city.vertex for city in connected_cities]
-        next_city = route[index + 1] if index + 1 < len(route) else None
-        # if end of list, break
-        if not next_city:
+    for idx, city in enumerate(route):
+        connections = graph.get_neighbors(city)
+        next_city = route[idx + 1] if idx + 1 < len(route) else None
+
+        if next_city == None:
             break
-        # next city isn't in the list, return false
-        if next_city not in route_cities:
+
+        elif next_city not in [e.vertex for e in connections]:
             return False, 0
-        # find city in neighbors list and add weight
-        for connection in connected_cities:
-            if connection.vertex == next_city:
-                total_weight += connection.weight
-        
-    return True, total_weight 
+
+        next_city_weight = sum([e.weight for e in connections if e.vertex == next_city])
+        total_weight += next_city_weight
+
+    return True, total_weight
